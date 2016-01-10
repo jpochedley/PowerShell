4ccec116ce24cc4797f00160d08db6f1c6c03a92
Adrian Rodriguez <adrian@rdrgz.net>
2016-01-10 11:36:38 -0700

    Updated test to only run PSScriptAnalyzer Pester test if PSVersion is 5 or above.

--- Tests/PowerShell.Tests.ps1
+++ Tests/PowerShell.Tests.ps1
@@ -1,40 +1,42 @@
 ﻿$Overrides = @{}
 
-Describe "Adrian's PowerShell Scripts"{
-    Import-Module -Name "$PSScriptRoot\PSScriptAnalyzer\1.2.0\PSScriptAnalyzer.psd1"
+If($PSVersionTable.PSVersion.Major -ge 5){
+    Describe "Adrian's PowerShell Scripts"{
+        Import-Module -Name "$PSScriptRoot\PSScriptAnalyzer\1.2.0\PSScriptAnalyzer.psd1"
     
-    $ItemsToEvaluate = Get-ChildItem -Path "$PSScriptRoot\..\" -Recurse -File -Include *.ps1, *psm1, *psd1, *ps1xml | Where-Object -Property Directory -NotMatch -Value 'Tests' 
+        $ItemsToEvaluate = Get-ChildItem -Path "$PSScriptRoot\..\" -Recurse -File -Include *.ps1, *psm1, *psd1, *ps1xml | Where-Object -Property Directory -NotMatch -Value 'Tests' 
 
-    Foreach($Item in $ItemsToEvaluate)
-    {
-        It "$($Item.Name) conforms to PowerShell best practices." {
-            $BPAVioloations = Invoke-ScriptAnalyzer -Path $Item.FullName -ExcludeRule PSUsePSCredentialType, PSAvoidUsingWMICmdlet -Severity Warning, Error
+        Foreach($Item in $ItemsToEvaluate)
+        {
+            It "$($Item.Name) conforms to PowerShell best practices." {
+                $BPAVioloations = Invoke-ScriptAnalyzer -Path $Item.FullName -ExcludeRule PSUsePSCredentialType, PSAvoidUsingWMICmdlet -Severity Warning, Error
 
-            Foreach($Violation in $BPAVioloations)
-            {
-                If($Overrides.Keys -contains $Violation.RuleName)
-                { 
-                    $Violation | 
-                    Where-Object -FilterScript {
-                        $Overrides.$($Violation.RuleName) -notmatch $Violation.FileName
-                    } | 
-                    Select-Object -ExpandProperty RuleName | 
-                    ForEach-Object -Process {
-                        Write-Warning -Message "$_ rule violation: $($Item.FullName)"
-                        
-                        $Violation |
-                        Should Be $Null
-                    }
-                }
-                Else
+                Foreach($Violation in $BPAVioloations)
                 {
-                    $Violation | 
-                    Select-Object -ExpandProperty RuleName | 
-                    ForEach-Object -Process {
-                        Write-Warning -Message "$_ rule violation: $($Item.FullName)"
+                    If($Overrides.Keys -contains $Violation.RuleName)
+                    { 
+                        $Violation | 
+                        Where-Object -FilterScript {
+                            $Overrides.$($Violation.RuleName) -notmatch $Violation.FileName
+                        } | 
+                        Select-Object -ExpandProperty RuleName | 
+                        ForEach-Object -Process {
+                            Write-Warning -Message "$_ rule violation: $($Item.FullName)"
                         
-                        $Violation |
-                        Should Be $Null
+                            $Violation |
+                            Should Be $Null
+                        }
+                    }
+                    Else
+                    {
+                        $Violation | 
+                        Select-Object -ExpandProperty RuleName | 
+                        ForEach-Object -Process {
+                            Write-Warning -Message "$_ rule violation: $($Item.FullName)"
+                        
+                            $Violation |
+                            Should Be $Null
+                        }
                     }
                 }
             }
